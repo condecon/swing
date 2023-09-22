b418d1c077e52e3a41552d64c2853965f2a9f47f
Jonas Engicht <jonas.engicht@uni-jena.de>
2023-09-22 14:49:40 +0200

    moved utilities to typescript

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,27 +1,22 @@
 import _ from "lodash";
 
+
 /**
  * Return direct children elements.
- *
  * @see http://stackoverflow.com/a/27102446/368691
- * @param {HTMLElement} element
- * @returns {Array}
+ * @param element 
+ * @returns Array
  */
-const elementChildren = (element) => {
+export function elementChildren(element:HTMLElement):Array<object>{
 	return _.filter(element.childNodes, {
 		nodeType: 1,
 	});
-};
+}
 
 /**
  * @see http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript/4819886#4819886
- * @returns {boolean}
+ * @returns 
  */
-const isTouchDevice = () => {
-	return "ontouchstart" in window || navigator.msMaxTouchPoints;
-};
-
-export {
-	elementChildren,
-	isTouchDevice,
-};
+export function isTouchDevice():boolean{
+	return "ontouchstart" in window || navigator.maxTouchPoints > 0;
+}
